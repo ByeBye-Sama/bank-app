1c7ff8f7e17dfb1fc73a682b2aa466544f82b4cb
ByeBye-Sama <joaquin.byebye@gmail.com>
2019-04-04 18:12:22 -0500

    Finish main interface & reservation records

--- reservation.py
+++ reservation.py
@@ -23,13 +23,14 @@ class Reservation:
     observation = ""
 
     def __init__(self, idCustomer, idBarber, idService, reservationDate, observation):
-        try:
-            cursor.execute("INSERT INTO RESERVATION(idCustomer, idBarber, idService, reservationDate, observation) VALUES (" + str(
-                idCustomer) + "," + str(idBarber) + "," + str(idService) + "," + "'" + reservationDate + "'," + "'" + observation + "')")
-            db.commit()
-            print("Your reservation code is: ", cursor.lastrowid)
-        except sqlite3.IntegrityError as e:
-            print('sqlite error', e.args[0])
+        cursor.execute("INSERT INTO RESERVATION(idCustomer, idBarber, idService, reservationDate, observation) VALUES (" + str(
+            idCustomer) + "," + str(idBarber) + "," + str(idService) + "," + "'" + reservationDate + "'," + "'" + observation + "')")
+        db.commit()
+
+        print('\x1b[6;30;42m' + "|{:^100}|".format("SUCCESS")+'\x1b[0m')
+        print("+{:-<100}+".format(""))
+        print("Your reservation code is: ", cursor.lastrowid)
+        print("+{:-<100}+".format(""))
 
     def makeReservation(self):
 
@@ -46,6 +47,34 @@ class Reservation:
         reservationDate = input("Insert reservation date: ")
         print("+{:-<100}+".format(""))
         observation = input("Insert observations: ")
+        print("+{:-<100}+".format(""))
 
         Reservation.__init__('', idCustomer, idBarber,
                              idService, reservationDate, observation)
+
+    def reservationScore(self):
+
+        print("+{:-<100}+".format(""))
+        print("|{:^100}|".format("RESERVATION RECORDS"))
+        print("+{:-<100}+".format(""))
+
+        firstDate = input("Insert first date with format (DD/MM/YY): ")
+        lastDate = input("Insert first date with format (DD/MM/YY): ")
+
+        print("+{:-<100}+".format(""))
+        print('\x1b[6;30;42m' + "|{:^100}|".format("SUCCESS")+'\x1b[0m')
+
+        reservations = "SELECT c.nameCustomer Customer, b.nameBarber Barber, s.nameService Service, r.reservationDate , r.observation FROM RESERVATION r INNER JOIN CUSTOMER c on r.idCustomer=c.idCustomer INNER JOIN BARBER b on r.idBarber=b.idBarber INNER JOIN SERVICE s on r.idService=s.idService WHERE r.reservationDate BETWEEN ? and ?;"
+        cursor.execute(reservations, [(firstDate), (lastDate)])
+        allreservations = cursor.fetchall()
+
+        print("+{:-<18}+{:-<18}+{:-<18}+{:-<18}+{:-<24}+".format("", "", "", "", ""))
+        print("|{:^18}|{:^18}|{:^18}|{:^18}|{:^24}|".format(
+            "Customer", "Barber", "Service", "Date", "Observation"))
+        print("+{:-<18}+{:-<18}+{:-<18}+{:-<18}+{:-<24}+".format("", "", "", "", ""))
+
+        for Customer, Barber, Service, reservationDate, observation in allreservations:
+            print("|{:^18}|{:^18}|{:^18}|{:^18}|{:^24}|".format(
+                Customer, Barber, Service, reservationDate, observation))
+            print(
+                "+{:-<18}+{:-<18}+{:-<18}+{:-<18}+{:-<24}+".format("", "", "", "", ""))
